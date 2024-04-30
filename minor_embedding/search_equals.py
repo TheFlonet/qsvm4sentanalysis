dd26e9db7a21e9d51acf20094d3c5b18c3fcacff
Mario Bifulco <48624954+TheFlonet@users.noreply.github.com>
2024-04-30 15:53:23 +0200

    Added same shape checking for embedded graph on toy problem

--- minor_embedding/search_equals.py
+++ minor_embedding/search_equals.py
@@ -1,8 +1,14 @@
 import networkx as nx
 from minorminer import find_embedding
+from sklearn.metrics import mean_squared_error
+
 from minor_embedding.architectures import toy_svm
 from minor_embedding.minors import generate_pegasus
 import dwave_networkx as dnx
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
 
 
 def generate_embedding_graph(source: nx.Graph, target: nx.Graph, seed: int) -> nx.Graph:
@@ -13,7 +19,7 @@ def generate_embedding_graph(source: nx.Graph, target: nx.Graph, seed: int) -> n
     return dnx.pegasus_graph(16, node_list=embedding_i_nodes)
 
 
-def main() -> None:
+def same_graph_same_location() -> None:
     pegasus = generate_pegasus()
     svm = toy_svm()
 
@@ -31,27 +37,39 @@ def main() -> None:
             if embeddings[i].adj == embeddings[j].adj:
                 print(f'Embeddings for {primes[i]} and {primes[j]} are identical')
 
-    '''
-    Results:
-    
-    Embeddings for 19 and 6451 are identical
-    Embeddings for 107 and 4871 are identical
-    Embeddings for 419 and 6947 are identical
-    Embeddings for 859 and 2789 are identical
-    Embeddings for 967 and 7523 are identical
-    Embeddings for 1237 and 6967 are identical
-    Embeddings for 1699 and 6703 are identical
-    Embeddings for 1709 and 4789 are identical
-    Embeddings for 1979 and 5107 are identical
-    Embeddings for 2389 and 3761 are identical
-    Embeddings for 2917 and 3041 are identical
-    Embeddings for 3571 and 3793 are identical
-    Embeddings for 3877 and 6709 are identical
-    Embeddings for 4027 and 6199 are identical
-    Embeddings for 4813 and 7789 are identical
-    Embeddings for 5179 and 6737 are identical
-    '''
+
+def same_graph() -> None:
+    pegasus = generate_pegasus()
+    svm = toy_svm()
+
+    with open('../util/1000.prime') as f:
+        primes = [int(x) for x in f.readline().split(', ')]
+    print('Seeds loaded, generating embedding...')
+    embeddings = [generate_embedding_graph(svm, pegasus, i) for i in primes]
+
+    print('Saving embeddings to disk')
+    os.makedirs('embedded_toy_graph', exist_ok=True)
+    for i, e in enumerate(embeddings):
+        fig = plt.figure()
+        dnx.draw_pegasus(e, crosses=True, with_labels=False, ax=fig.add_subplot())
+        fig.savefig(f'embedded_toy_graph/{primes[i]}.png', format='png')
+        plt.close(fig)
+    imgs = [cv2.cvtColor(cv2.imread(f'embedded_toy_graph/{prime}.png'), cv2.COLOR_BGR2GRAY) for prime in primes]
+
+    s = ''
+    for i in tqdm(range(len(primes))):
+        for j in range(len(primes)):
+            if j <= i:
+                continue
+            mse = mean_squared_error(imgs[i], imgs[j])
+            if mse == 0:
+                s += f'{primes[i]} - {primes[j]} have the same embedding\n'
+    with open('same_shape.log', 'w') as f:
+        f.write(s)
+
+    os.removedirs('embedded_toy_graph')
 
 
 if __name__ == '__main__':
-    main()
+    # same_graph_same_location()
+    same_graph()
