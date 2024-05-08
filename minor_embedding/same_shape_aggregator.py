1c72657fd56cb0cf9d862307fac070c99aedd75b
Mario Bifulco <48624954+TheFlonet@users.noreply.github.com>
2024-05-08 15:01:24 +0200

    Update logbook + aggregated same shape results

---
+++ minor_embedding/same_shape_aggregator.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def aggregate():
+    with open('./same_shape.log') as f:
+        results = [x.strip() for x in f.readlines()]
+
+    pairs = []
+    for r in results:
+        n1, n2 = int(r.split()[0]), int(r.split()[2])
+        pairs.append((n1, n2))
+
+    groups = []
+    for pair in pairs:
+        added = False
+        for group in groups:
+            if pair[0] in group:
+                group.add(pair[1])
+                added = True
+        if not added:
+            s = set()
+            s.add(pair[0])
+            s.add(pair[1])
+            groups.append(s)
+
+    df = []
+    for group in groups:
+        df.append((len(group), sorted(list(group))))
+    df = pd.DataFrame(df, columns=['Num elems', 'elements'])
+
+    df.sort_values(by='Num elems', ascending=False).to_csv('shape.csv', index=False)
+
+
+if __name__ == '__main__':
+    aggregate()
