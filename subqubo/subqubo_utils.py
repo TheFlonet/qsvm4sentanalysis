56c391a85539506b9fe8f38d60840225a95c5fa4
Mario Bifulco <48624954+TheFlonet@users.noreply.github.com>
2024-06-12 16:16:00 +0200

    SubQubo split
    Missing: exhaustive search in case of conflict

--- subqubo/subqubo_utils.py
+++ subqubo/subqubo_utils.py
@@ -1,24 +1,186 @@
-from typing import List, Mapping, Hashable
+import logging
+from collections import defaultdict
+from typing import List, Mapping, Hashable, Tuple
+import networkx as nx
+import numpy as np
+import pandas as pd
 from dimod import SampleSet
+from dimod.views.samples import SampleView
 from dwave.samplers import SimulatedAnnealingSampler
 from numpy import integer, floating
+from scipy.linalg import lu
+
+log = logging.getLogger('subqubo')
 
 
-def split_problem(qubo: Mapping[tuple[Hashable, Hashable], float | floating | integer],
-                  halt_dim: int) -> List[Mapping[tuple[Hashable, Hashable], float | floating | integer]]:
-    pass
+def __split_problem(qubo: Mapping[tuple[Hashable, Hashable], float | floating | integer], dim: int) -> Tuple[
+    Mapping[tuple[Hashable, Hashable], float | floating | integer],
+    Mapping[tuple[Hashable, Hashable], float | floating | integer],
+    Mapping[tuple[Hashable, Hashable], float | floating | integer]
+]:
+    """
+        Returns 3 sub-problems in qubo form.
+        The 3 sub-problems correspond to the matrices obtained by dividing the qubo matrix of the original problem
+        in half both horizontally and vertically.
+        The sub-problem for the sub-matrix in the bottom left corner is not given as this is always empty.
+        The order of the results is:
+        - Upper left sub-matrix,
+        - Upper right sub-matrix,
+        - Lower right sub-matrix.
+
+        All sub-problems are converted to obtain an upper triangular matrix.
+    """
+    split_u_l = defaultdict(int)
+    split_u_r = defaultdict(int)
+    split_d_r = defaultdict(int)
+    split_idx = dim // 2
+    qubo_matrix_u_r = np.zeros((split_idx, split_idx))
+
+    for k, v in qubo.items():
+        row, col = k[0] - 1, k[1] - 1
+        if row < split_idx and col < split_idx:
+            split_u_l[k] = v
+        elif row < split_idx <= col:
+            qubo_matrix_u_r[row, col - split_idx] = v
+        elif row >= split_idx and col >= split_idx:
+            split_d_r[k] = v
+        else:
+            raise ValueError('All values in the lower left matrix should be 0, so not present in the qubo dictionary')
+
+    qubo_matrix_u_r = lu(qubo_matrix_u_r, permute_l=True)[1]
+    for i in range(qubo_matrix_u_r.shape[0]):
+        for j in range(qubo_matrix_u_r.shape[1]):
+            if qubo_matrix_u_r[i, j] != 0:
+                split_u_r[(i + 1, j + 1 + split_idx)] = qubo_matrix_u_r[i, j]
+
+    return split_u_l, split_u_r, split_d_r
 
 
-def aggregate_solutions(solutions: List[SampleSet]) -> SampleSet:
-    pass
+def __nan_hamming_distance(a, b):
+    mask = ~np.isnan(a) & ~np.isnan(b)
+    if np.sum(mask) == 0:
+        return np.inf
+    return np.sum(a[mask] != b[mask]) / np.sum(mask)
+
+
+def __combine_rows(row1, row2):
+    combined_row = []
+    is_row_consistent = True
+    for col in row1.index:
+        val1, val2 = row1[col], row2[col]
+        if col == 'energy':
+            if is_row_consistent:
+                combined_row.append(val1 + val2)
+            else:
+                combined_row.append(np.nan)
+        else:
+            if pd.isna(val2):
+                combined_row.append(val1)
+            elif val1 == val2:
+                combined_row.append(val1)
+            else:
+                combined_row.append(np.nan)
+                is_row_consistent = is_row_consistent and False
+    return combined_row
+
+
+def __combine_ul_lr(ul_qubo: pd.DataFrame, lr_qubo: pd.DataFrame) -> pd.DataFrame:
+    ul_qubo['tmp'] = 1
+    lr_qubo['tmp'] = 1
+    merge = pd.merge(ul_qubo, lr_qubo, on='tmp')
+    merge['energy'] = merge['energy_x'] + merge['energy_y']
+    return merge.drop(['energy_x', 'energy_y', 'tmp'], axis=1)
+
+
+def __fill_ur_qubo(schema: pd.DataFrame, df_to_fill: pd.DataFrame) -> pd.DataFrame:
+    missing_columns = set(schema.columns) - set(df_to_fill.columns)
+    for col in missing_columns:
+        df_to_fill[col] = np.nan
+    return df_to_fill[schema.columns]
+
+
+def __get_closest_assignments(starting_sols: pd.DataFrame, ur_qubo_filled: pd.DataFrame) -> pd.DataFrame:
+    closest_rows = []
+    for i, row in starting_sols.iterrows():
+        distances = []
+        for j, sol_row in ur_qubo_filled.iterrows():
+            distance = __nan_hamming_distance(row.values, sol_row.values)
+            distances.append(distance)
+        closest_idx = np.argmin(distances)
+        closest_rows.append(ur_qubo_filled.iloc[closest_idx])
+    return pd.DataFrame(closest_rows).reset_index(drop=True)
+
+
+def __aggregate_solutions(solutions: List[pd.DataFrame], qubo_matrix: np.ndarray) -> pd.DataFrame:
+    # Aggregate upper-left qubo with lower-right
+    starting_sols = __combine_ul_lr(solutions[0], solutions[2])
+    # Set missing columns in upper-right qubo to NaN
+    ur_qubo_filled = __fill_ur_qubo(starting_sols, solutions[1])
+    # Search the closest assignments between upper-right qubo and merged solution (UL and LR qubos)
+    closest_df = __get_closest_assignments(starting_sols, ur_qubo_filled)
+
+    # Combine
+    combined_df = pd.DataFrame([__combine_rows(row1, row2) for (_, row1), (_, row2) in
+                                zip(starting_sols.iterrows(), closest_df.iterrows())],
+                               columns=starting_sols.columns)
+
+    trials = 1 << combined_df.drop(columns=['energy']).isna().sum().sum()
+    # Brute force resolution
+    print(combined_df)
+    log.info(f'Conflicts resolved with {trials} classic resolutions' if trials > 0
+             else 'No conflict, merge successfully done')
+
+    exit()
+
+    return combined_df
 
 
 def subqubo_solve(sampler: SimulatedAnnealingSampler,
-                  qubo: Mapping[tuple[Hashable, Hashable], float | floating | integer]) -> SampleSet:
-    qubos = split_problem(qubo, 2)
-    solutions = []
+                  qubo: Mapping[tuple[Hashable, Hashable], float | floating | integer], dim: int) -> pd.DataFrame:
+    if dim == 2:
+        res = (sampler.sample_qubo(qubo, num_reads=10)
+               .to_pandas_dataframe()
+               .drop(columns=['num_occurrences'])
+               .drop_duplicates()
+               .sort_values(by='energy', ascending=True))
+        return res[res['energy'] == min(res['energy'])]
 
-    for q in qubos:
-        solutions.append(sampler.sample_qubo(q))
+    qubo_matrix = np.zeros((dim, dim))
+    for k, v in qubo.items():
+        qubo_matrix[k[0] - 1, k[1] - 1] = v
 
-    return aggregate_solutions(solutions)
+    return __aggregate_solutions([subqubo_solve(sampler, q, dim // 2) for q in __split_problem(qubo, dim)], qubo_matrix)
+
+
+def print_cut_from_sampleset(sampleset: SampleSet) -> None:
+    """
+    Adapted from the example available on the dwave GitHub profile:
+
+    Available here: https://github.com/dwave-examples/maximum-cut/blob/master/maximum_cut.py
+    """
+    print('-' * 60)
+    print(f'{"Set 0":>15s}{"Set 1":>15s}{"Energy":^15s}{"Cut Size":^15s}')
+    print('-' * 60)
+    for sample, E in sampleset.data(fields=['sample', 'energy']):
+        S0 = [k for k, v in sample.items() if v == 0]
+        S1 = [k for k, v in sample.items() if v == 1]
+        print(f'{str(S0):>15s}{str(S1):>15s}{str(E):^15s}{str(int(-1 * E)):^15s}')
+
+
+def draw_cut_from_sample(graph: nx.Graph, sample: SampleView) -> None:
+    """
+    Adapted from the example available on the dwave GitHub profile:
+
+    Available here: https://github.com/dwave-examples/maximum-cut/blob/master/maximum_cut.py
+    """
+    S0 = [node for node in graph.nodes if not sample[node]]
+    S1 = [node for node in graph.nodes if sample[node]]
+    cut_edges = [(u, v) for u, v in graph.edges if sample[u] != sample[v]]
+    uncut_edges = [(u, v) for u, v in graph.edges if sample[u] == sample[v]]
+
+    pos = nx.spectral_layout(graph)
+    nx.draw_networkx_nodes(graph, pos, nodelist=S0, node_color='r')
+    nx.draw_networkx_nodes(graph, pos, nodelist=S1, node_color='c')
+    nx.draw_networkx_edges(graph, pos, edgelist=cut_edges, style='dashdot', alpha=0.5, width=3)
+    nx.draw_networkx_edges(graph, pos, edgelist=uncut_edges, style='solid', width=3)
+    nx.draw_networkx_labels(graph, pos)
--- subqubos.py
+++ subqubos.py
@@ -1,30 +1,53 @@
-import dimod
+import logging
+from collections import defaultdict
+from typing import Mapping, Hashable, Tuple
 import networkx as nx
-from dimod import SampleSet
+import pandas as pd
 from dwave.samplers import SimulatedAnnealingSampler
+from matplotlib import pyplot as plt
+from numpy import floating, integer
 from subqubo.subqubo_utils import subqubo_solve
 
 
-def generate_max_cut_problem() -> nx.Graph:
-    # Generate a graph that has at least 2 equivalent solutions to max-cut problem
-    # The problem will generate a qubo matrix 2^n X 2^n
-    pass
+def generate_max_cut_problem() -> Tuple[nx.Graph, Mapping[tuple[Hashable, Hashable], float | floating | integer]]:
+    graph = nx.Graph()
+    graph.add_edges_from([(1, 2), (1, 3), (2, 4), (3, 4), (3, 5), (4, 5), (1, 6),
+                          (2, 6), (4, 7), (1, 7), (2, 8), (3, 8), (1, 4), (2, 3)])
+
+    qubo = defaultdict(int)
+    for i, j in graph.edges:
+        qubo[(i, i)] += -1
+        qubo[(j, j)] += -1
+        qubo[(i, j)] += 2
+    return graph, qubo
 
 
-def compare_solutions(ground_truth: SampleSet, sol: SampleSet):
+def compare_solutions(ground_truth: pd.DataFrame, sol: pd.DataFrame,
+                      qubo: Mapping[tuple[Hashable, Hashable], float | floating | integer]):
+    print(ground_truth)
+    print(sol)
     pass
 
 
 def main() -> None:
-    graph = generate_max_cut_problem()
-    bqm = dimod.from_networkx_graph(graph)
+    graph, qubo = generate_max_cut_problem()
+
+    nx.draw(graph, with_labels=True, pos=nx.spectral_layout(graph))
+    plt.savefig("graph.png", format="PNG")
 
     sampler = SimulatedAnnealingSampler()
-    direct_solutions = sampler.sample(bqm)
-    subqubos_solutions = subqubo_solve(sampler, bqm.to_qubo())
+    direct_solutions = sampler.sample_qubo(qubo, num_reads=10)
+    subqubos_solutions = subqubo_solve(sampler, qubo, len({i for k in qubo.keys() for i in k}))
 
-    compare_solutions(direct_solutions, subqubos_solutions)
+    compare_solutions(direct_solutions.to_pandas_dataframe(), subqubos_solutions, qubo)
 
 
 if __name__ == '__main__':
+    log = logging.getLogger('subqubo')
+    log.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
     main()
